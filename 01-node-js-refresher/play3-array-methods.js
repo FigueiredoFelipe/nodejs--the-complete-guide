c5f28cd8a3fd9567e4245f6effe1118b4dd07775
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-03-14 14:28:00 -0300

    Create play3-array-methods.js

---
+++ 01-node-js-refresher/play3-array-methods.js
@@ -0,0 +1,12 @@
+// ABOUT FELIPE FIGUEIREDO:
+// https://www.linkedin.com/in/fjnfigueiredo/
+// https://github.com/FigueiredoFelipe
+
+const hobbies = ['Sports', 'Cooking']
+
+// for (hobby of hobbies){
+//     console.log(hobby);
+// }
+
+console.log(hobbies.map(hobby => 'Hobby: '+ hobby));
+console.log(hobbies);
